8bb65534f6977a4732e0c70e27fe8060b8890d7b
Kuroro <leandro.qnj@gmail.com>
2022-07-30 09:53:11 -0300

    Criando mais uma linha de String

--- src/GuanabaraEstudos.java
+++ src/GuanabaraEstudos.java
@@ -4,6 +4,7 @@ public class GuanabaraEstudos {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Agradecimento aos mestres do commit");
 	}
 	
 	
